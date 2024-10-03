7af60f98f202f1f504cd86fcf6c53ef12ea08288
TaskMagicKyle <kyle@taskmagic.com>
2024-10-02 23:45:32 -0700

    Style + Format

--- scripts/background.js
+++ scripts/background.js
@@ -12,6 +12,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       })
     })
 
-    return true 
+    return true
   }
 })
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,24 @@
-document.getElementById('copyButton').addEventListener('click', async () => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    const currentTabUrl = tabs[0].url;
-    const tabId = tabs[0].id; 
-    chrome.runtime.sendMessage({action: "getCookies", url: currentTabUrl, tabId}, async (response) => {
-      try {
-        await navigator.clipboard.writeText(response.cookies);
-      } catch (error) {
-        console.error('Failed to copy cookies:', error);
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const url = new URL(tabs[0].url)
+    const hostname = url.hostname
+    document.getElementById("hostname").textContent = `from ${hostname}`
+  })
+})
+
+document.getElementById("copyButton").addEventListener("click", async () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTabUrl = tabs[0].url
+    const tabId = tabs[0].id
+    chrome.runtime.sendMessage(
+      { action: "getCookies", url: currentTabUrl, tabId },
+      async (response) => {
+        try {
+          await navigator.clipboard.writeText(response.cookies)
+        } catch (error) {
+          console.error("Failed to copy cookies:", error)
+        }
       }
-    });
-  });
-});
+    )
+  })
+})
