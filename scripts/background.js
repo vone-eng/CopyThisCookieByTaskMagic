22f957eb987589e9af1e89e1ac0dac53264d06e6
TaskMagicKyle <kyle@taskmagic.com>
2024-10-02 23:20:05 -0700

    rm content, navigator was accessible in popup

--- scripts/background.js
+++ scripts/background.js
@@ -15,14 +15,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true 
   }
 })
-
-// Send to content.js
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  chrome.tabs.sendMessage(
-    tabs[0].id,
-    {
-      message: "copyText",
-      textToCopy: "some text",
-    },
-  )
-})
